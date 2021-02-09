d240d23a3e3af42bd7211f317235196bb0b5def5
Francisco Aramburu <35516858+FranciscoBuru@users.noreply.github.com>
2021-02-09 15:18:06 -0600

    T1

---
+++ Tareas/T1/T1LISP.lisp
@@ -0,0 +1,32 @@
+; Para cargar el archivo con las funciones correr 
+;(load "T1LISP.lisp" :print t)
+; en un lisp ejecutado en el directorio de este archivo. 
+
+;;Inverso lista sin mapcar
+(defun B (lst)
+	(cond 
+		((atom lst) lst)
+		(t (append (B (cdr lst)) (B (car lst))))
+		
+	)
+)
+
+;; Suma a profundidad
+
+(defun C(lst)
+	(cond 
+		((null lst) 0)
+		((atom lst) lst)
+		(t (+ (C (cdr lst)) (C (car lst))))
+	)
+)
+
+;;Suma con mapcar
+(setq x 0)
+(defun D(lst)
+	(cond 
+		((listp lst) (mapcar #'D lst)) 
+		(t (incf x lst))
+	)
+)
+
